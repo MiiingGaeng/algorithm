e7019da6d1615a6d40cf1ce8827a0773d4843abb
miing_gaeng <pem4134@naver.com>
2024-12-29 23:13:24 +0900

    33_약수갯수덧셈해결

--- algorism.js
+++ algorism.js
@@ -1,16 +1,34 @@
 //32 내적
 // 길이가 같은 두 1차원 정수 배열 a, b가 매개변수로 주어집니다. a와 b의 내적을 return 하도록 solution 함수를 완성해주세요. 이때, a와 b의 내적은 a[0]*b[0] + a[1]*b[1] + ... + a[n-1]*b[n-1] 입니다. (n은 a, b의 길이)
 
-function solution(a, b) {
-  var answer = 0;
+// function solution(a, b) {
+//   var answer = 0;
   
-  for(let i=0; i<a.length; i++){
-      answer += a[i]*b[i];
-  }
+//   for(let i=0; i<a.length; i++){
+//       answer += a[i]*b[i];
+//   }
   
-  return answer
-};
+//   return answer
+// };
 
 //33 약수의 개수와 덧셈
 // 두 정수 left와 right가 매개변수로 주어집니다. left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
 
+function solution(left, right) {
+  let answer = 0;
+  //left ~ right 반복
+  for(let i=left; i<= right; i++){
+      let count = 0;
+      //j = i의 약수
+      for(let j=1; j<=i; j++){
+          if(i%j===0){
+              count ++;
+          }
+      }
+      
+      //약수의 수 = 짝수 조건문
+      (count % 2 === 0) ? answer+=i : answer-=i;
+  }
+  
+  return answer
+}
