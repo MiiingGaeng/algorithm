6674565c189498419b816722c2887652a9e35e60
miing_gaeng <pem4134@naver.com>
2024-12-30 09:54:44 +0900

    36_문자열해결

--- algorism.js
+++ algorism.js
@@ -76,3 +76,33 @@
 //36 문자열 다루기 기본
 // 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요. 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
 
+//왜 안되지...
+function solution(s) {
+  if(s.length == 4 || s.length == 6){
+    return !Number.isNaN(Number(s))
+  }else{
+    return false;
+  }
+};
+
+function solution(s) {
+  if(s.length == 4 || s.length == 6){
+    var answer = true;
+    var arr = s.split('');
+  
+    for(let i=0; i<arr.length; i++){
+      if(Number.isNaN(Number(arr[i])) === true){
+        answer = false;
+        break;
+      }
+    }
+  
+    return answer;
+  } else {
+    return false;
+  }
+}
+
+
+
+//37 
