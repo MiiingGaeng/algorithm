8a7b4afb68dc172d6419b03b3746edbaf48b122d
miing_gaeng <pem4134@naver.com>
2024-12-30 12:02:19 +0900

    37_행렬덧셈해결

--- algorism.js
+++ algorism.js
@@ -84,6 +84,8 @@
 //     return false;
 //   }
 // };
+//e = 자연상수 (Math.E) 값으로 인식된다
+//그래서 '12e1'가 true로 떠서 실패
 
 // function solution(s) {
 //   if(s.length == 4 || s.length == 6){
@@ -108,3 +110,18 @@
 //37 행렬의 덧셈
 // 행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
 
+function solution(arr1, arr2) {
+  var answer = [];
+  
+  //arr1,2의 배열(index i) 불러오기
+  for(let i=0; i < arr1.length; i++){
+      let test = [];
+      //arr1,2의 배열(index i)의 인덱스 값(index j) 불러오기
+      for(let j=0; j<arr1[i].length; j++){
+          test.push(arr1[i][j] + arr2[i][j])
+      }
+      answer.push(test)
+  }
+  
+  return answer;
+}
