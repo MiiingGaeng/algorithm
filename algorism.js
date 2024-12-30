dec08e8e4ca65bb4d302b77a17a80181109a6aa1
miing_gaeng <pem4134@naver.com>
2024-12-30 09:16:21 +0900

    36_문자열문제

--- algorism.js
+++ algorism.js
@@ -56,17 +56,23 @@
 //35 부족한 금액 계산하기
 // 새로 생긴 놀이기구는 인기가 매우 많아 줄이 끊이질 않습니다. 이 놀이기구의 원래 이용료는 price원 인데, 놀이기구를 N 번 째 이용한다면 원래 이용료의 N배를 받기로 하였습니다. 즉, 처음 이용료가 100이었다면 2번째에는 200, 3번째에는 300으로 요금이 인상됩니다. 놀이기구를 count번 타게 되면 현재 자신이 가지고 있는 금액에서 얼마가 모자라는지를 return 하도록 solution 함수를 완성하세요. 단, 금액이 부족하지 않으면 0을 return 하세요.
 
-function solution(price, money, count) {
-  let totalPrice = 0;
+// function solution(price, money, count) {
+//   let totalPrice = 0;
   
-  for(let i=1; i <= count; i++){
-      totalPrice += price * i;
-  }
+//   for(let i=1; i <= count; i++){
+//       totalPrice += price * i;
+//   }
   
-  //조건문 추가!
-  if(totalPrice > money){
-   return parseInt(totalPrice - money);   
-  } else{
-  return 0;
-  }
-}
+//   //조건문 추가!
+//   if(totalPrice > money){
+//    return parseInt(totalPrice - money);   
+//   } else{
+//   return 0;
+//   }
+// }
+
+
+
+//36 문자열 다루기 기본
+// 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요. 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+
