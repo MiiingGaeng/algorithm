f4c094a811df3ccdb7f578009d2cb169fac06e71
miing_gaeng <pem4134@naver.com>
2024-12-31 10:54:38 +0900

    38 직사각형별해결

--- algorism.js
+++ algorism.js
@@ -118,3 +118,18 @@
 
 //38 직사각형 별찍기
 // 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다. 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+  //data = n,m
+  const num = data.split(' ');
+  //n과 m 값을 숫자로
+  const a = Number(num[0]),
+    b = Number(num[1]);
+
+  //세로값(m) 반복
+  for (let i = 0; i < b; i++) {
+    //가로값(n)만큼 별 출력
+    console.log('*'.repeat(a));
+  }
+});
