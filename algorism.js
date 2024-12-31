1ce12f721e141c9afcabdd65c5cc5b6db89fa080
miing_gaeng <pem4134@naver.com>
2024-12-31 11:27:13 +0900

    40 3진법해결

--- algorism.js
+++ algorism.js
@@ -146,3 +146,12 @@
 
 //40 3진법 뒤집기
 // 자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+
+function solution(n) {
+  //3진수 배열 만들고 뒤집고 합체
+  var arrN3 = [...n.toString(3)];
+  var revN3 = arrN3.reverse().join('');
+
+  //3진수를 10진수로 만들기
+  return parseInt(revN3, 3);
+}
