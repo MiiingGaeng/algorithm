39c228fcb99d0d811e1a5f7960091dc2c43cb76a
miing_gaeng <pem4134@naver.com>
2024-12-31 17:29:52 +0900

    과제1해결

--- algorism.js
+++ algorism.js
@@ -158,3 +158,17 @@ function solution(n) {
 
 //과제1 문자열 연습하기
 // 대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+
+function solution(s) {
+  var arrS = s.toLowerCase().split('');
+  var numP = arrS.filter((item) => item === 'p');
+  var numY = arrS.filter((item) => item === 'y');
+
+  if (numP.length === 0 && numY.length === 0) {
+    return true;
+  } else if (numP.length === numY.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
