59188b6f0191a3d137a88ad23d27e2f4ca2a2d61
miing_gaeng <pem4134@naver.com>
2024-12-31 17:44:49 +0900

    과제2해결

--- algorism.js
+++ algorism.js
@@ -147,14 +147,14 @@
 //40 3진법 뒤집기
 // 자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
 
-function solution(n) {
-  //3진수 배열 만들고 뒤집고 합체
-  var arrN3 = [...n.toString(3)];
-  var revN3 = arrN3.reverse().join('');
+// function solution(n) {
+//   //3진수 배열 만들고 뒤집고 합체
+//   var arrN3 = [...n.toString(3)];
+//   var revN3 = arrN3.reverse().join('');
 
-  //3진수를 10진수로 만들기
-  return parseInt(revN3, 3);
-}
+//   //3진수를 10진수로 만들기
+//   return parseInt(revN3, 3);
+// }
 
 //과제1 문자열 연습하기
 // 대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
@@ -175,3 +175,17 @@ function solution(n) {
 
 //과제2 반복문,조건문 연습
 // 어떤 정수들이 있습니다. 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다. 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
+
+function solution(abs, signs) {
+  var answer = 0;
+
+  for (let i = 0; i < abs.length; i++) {
+    if (signs[i] === true) {
+      answer += abs[i];
+    } else {
+      answer -= abs[i];
+    }
+  }
+
+  return answer;
+}
