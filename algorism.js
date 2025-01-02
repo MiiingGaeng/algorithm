abe8fde7500634d93a748362086f930d7209e00e
miing_gaeng <pem4134@naver.com>
2025-01-02 13:38:19 +0900

    과제3 문자열정렬해결

--- algorism.js
+++ algorism.js
@@ -362,3 +362,26 @@
 
 //과제3 문자열 내 마음대로 정렬하기
 // 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
+
+function solution(strings, n) {
+  let answer = [];
+  let arr = [];
+
+  //sttings 요소 word 찾기
+  strings.forEach((word) => {
+    //word에 n번째 글자 찾기
+    let wordChar = word.charAt(n);
+    //해당인덱스 글자 앞에 붙이기
+    let indexWord = wordChar.concat(word);
+    arr.push(indexWord);
+  });
+  //정렬하기
+  arr = arr.sort();
+  //앞글자 삭제하기
+  arr.forEach((s) => {
+    let answerW = s.substring(1);
+    answer.push(answerW);
+  });
+
+  return answer;
+}
