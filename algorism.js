90a421b5a80ad3f2af9f076b61e725af7910665f
miing_gaeng <pem4134@naver.com>
2025-01-02 10:51:37 +0900

    45 숫자영단어해결

--- algorism.js
+++ algorism.js
@@ -312,3 +312,50 @@
 
 //45 숫자 문자열과 영단어
 //네오와 프로도가 숫자놀이를 하고 있습니다. 네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다.이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다. s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요.
+
+//테스트는 통과/결과는 실패
+// function solution(s) {
+//   var numToEng = [
+//     'zero',
+//     'one',
+//     'two',
+//     'three',
+//     'four',
+//     'five',
+//     'six',
+//     'seven',
+//     'eigth',
+//     'nine',
+//   ];
+
+//   for (let i = 0; i < 10; i++) {
+//     if (s.includes(numToEng[i])) {
+//       //replaceAll에서 일치항목을 찾기 때문에 if문이 필요 없다.
+//       s = s.replaceAll(numToEng[i], i);
+//     }
+//   }
+
+//   return Number(s);
+// }
+
+//해결
+function solution(s) {
+  var numToEng = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+
+  for (let i = 0; i < 10; i++) {
+    s = s.replaceAll(numToEng[i], i);
+  }
+
+  return Number(s);
+}
