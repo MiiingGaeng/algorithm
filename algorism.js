ddf4bb9d2ac165f6891f84985eaacee961092f87
miing_gaeng <pem4134@naver.com>
2025-01-03 10:34:46 +0900

    배열두배해결

--- algorism.js
+++ algorism.js
@@ -396,5 +396,14 @@
 // }
 
 //프로그래머스 2일차
-//분수의 덧셈
-// 첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1, 두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다. 두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+//배열 두배 만들기
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(numbers) {
+  let answer = [];
+  numbers.forEach((number) => {
+    answer.push(number * 2);
+  });
+
+  return answer;
+}
