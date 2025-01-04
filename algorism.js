9d2e8452b85340e5f3bae8523ed8048967bf5e73
miing_gaeng <pem4134@naver.com>
2025-01-04 13:58:39 +0900

    3일차 중앙값문제

--- algorism.js
+++ algorism.js
@@ -447,20 +447,24 @@
 //48 K번째수
 // 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다. 예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면 array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다. 1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다. 2에서 나온 배열의 3번째 숫자는 5입니다. 배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
 
-function solution(array, commands) {
-  let answer = [];
+// function solution(array, commands) {
+//   let answer = [];
 
-  commands.forEach((command) => {
-    let i = command[0] - 1;
-    let j = command[1];
-    let k = command[2] - 1;
+//   commands.forEach((command) => {
+//     let i = command[0] - 1;
+//     let j = command[1];
+//     let k = command[2] - 1;
 
-    let sliceArr = array.slice(i, j).sort((a, b) => a - b);
-    //sort()만 쓸 경우 문자열로 변환해서 정렬하기 때문에 오류가 발생할 수 있다.
-    //즉, [2, 3, 10]이 아닌 [10, 2, 3]으로 정렬될 가능성이 있다.
-    //따라서 정렬기준 함수를 같이 작성해주는 것이 더 정확하다.
-    answer.push(sliceArr[k]);
-  });
+//     let sliceArr = array.slice(i, j).sort((a, b) => a - b);
+//     //sort()만 쓸 경우 문자열로 변환해서 정렬하기 때문에 오류가 발생할 수 있다.
+//     //즉, [2, 3, 10]이 아닌 [10, 2, 3]으로 정렬될 가능성이 있다.
+//     //따라서 정렬기준 함수를 같이 작성해주는 것이 더 정확하다.
+//     answer.push(sliceArr[k]);
+//   });
 
-  return answer;
-}
+//   return answer;
+// }
+
+//프로그래머스 3일차
+//중앙값 구하기
+//중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다. 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다. 정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
