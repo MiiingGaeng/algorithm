95c631ade393290ea210622e6176a0f8443a56bf
miing_gaeng <pem4134@naver.com>
2025-01-04 13:51:37 +0900

    48 K번째수문제

--- algorism.js
+++ algorism.js
@@ -419,27 +419,30 @@
 //과제4 나이든 유저
 // 가장 아래의 코드가 실행 되었을 때, “Passed ~” 가 출력되도록 getAge 함수를 채워주세요
 
-var user = {
-  name: 'john',
-  age: 20,
-};
+// var user = {
+//   name: 'john',
+//   age: 20,
+// };
 
-var getAged = function (user, passedTime) {
-  // 여기를 작성해 주세요!
-};
+// var getAged = function (user, passedTime) {
+//   // 여기를 작성해 주세요!
+// };
 
-var agedUser = getAged(user, 6);
+// var agedUser = getAged(user, 6);
 
-var agedUserMustBeDifferentFromUser = function (user1, user2) {
-  if (!user2) {
-    console.log("Failed! user2 doesn't exist!");
-  } else if (user1 !== user2) {
-    console.log(
-      'Passed! If you become older, you will be different from you in the past!'
-    );
-  } else {
-    console.log('Failed! User same with past one');
-  }
-};
+// var agedUserMustBeDifferentFromUser = function (user1, user2) {
+//   if (!user2) {
+//     console.log("Failed! user2 doesn't exist!");
+//   } else if (user1 !== user2) {
+//     console.log(
+//       'Passed! If you become older, you will be different from you in the past!'
+//     );
+//   } else {
+//     console.log('Failed! User same with past one');
+//   }
+// };
 
-agedUserMustBeDifferentFromUser(user, agedUser);
+// agedUserMustBeDifferentFromUser(user, agedUser);
+
+//48 K번째수
+// 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다. 예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면 array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다. 1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다. 2에서 나온 배열의 3번째 숫자는 5입니다. 배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
