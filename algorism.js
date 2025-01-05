9167e2e19d4d39af69c7337827f74e6da3ec2e9f
miing_gaeng <pem4134@naver.com>
2025-01-05 18:07:26 +0900

    4일차 편지

--- algorism.js
+++ algorism.js
@@ -623,16 +623,23 @@
 //양꼬치
 //머쓱이네 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스로 줍니다. 양꼬치는 1인분에 12,000원, 음료수는 2,000원입니다. 정수 n과 k가 매개변수로 주어졌을 때, 양꼬치 n인분과 음료수 k개를 먹었다면 총얼마를 지불해야 하는지 return 하도록 solution 함수를 완성해보세요.
 
-function solution(n, k) {
-  let totalDrinks = 0;
-  let howMany = Math.floor(n / 10);
+// function solution(n, k) {
+//   let totalDrinks = 0;
+//   let howMany = Math.floor(n / 10);
 
-  n >= 10 ? (totalDrinks = k - howMany) : (totalDrinks = k);
+//   n >= 10 ? (totalDrinks = k - howMany) : (totalDrinks = k);
 
-  return n * 12000 + totalDrinks * 2000;
-}
+//   return n * 12000 + totalDrinks * 2000;
+// }
 
-//다른사람 풀이
-function solution(n, k) {
-  return n * 12000 + k * 2000 - parseInt(n / 10) * 2000;
+// //다른사람 풀이
+// function solution(n, k) {
+//   return n * 12000 + k * 2000 - parseInt(n / 10) * 2000;
+// }
+
+//편지
+// 머쓱이는 할머니께 생신 축하 편지를 쓰려고 합니다. 할머니가 보시기 편하도록 글자 한 자 한 자를 가로 2cm 크기로 적으려고 하며, 편지를 가로로만 적을 때, 축하 문구 message를 적기 위해 필요한 편지지의 최소 가로길이를 return 하도록 solution 함수를 완성해주세요.
+
+function solution(message) {
+  return 2 * message.length;
 }
