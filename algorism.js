8724a2a29421cf6db2ea7670140d98d603b5bede
miing_gaeng <pem4134@naver.com>
2025-01-05 18:14:54 +0900

    4일차 순서쌍의개수

--- algorism.js
+++ algorism.js
@@ -647,7 +647,24 @@
 //문자열 뒤집기
 // 문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
 
-function solution(string) {
-  let arr = string.split('');
-  return arr.reverse().join('');
+// function solution(string) {
+//   let arr = string.split('');
+//   return arr.reverse().join('');
+// }
+
+// //한줄로 작성해보기
+// function solution(string) {
+//   return string.split('').reverse().join('');
+// }
+
+//순서쌍의 개수
+//순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다. 자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 return하도록 solution 함수를 완성해주세요.
+
+function solution(n) {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) arr.push(i);
+  }
+
+  return arr.length;
 }
