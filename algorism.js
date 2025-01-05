1b00cb7ab8bb9a75676a0d1479058232d731bff8
miing_gaeng <pem4134@naver.com>
2025-01-05 18:06:05 +0900

    4일차 양꼬치

--- algorism.js
+++ algorism.js
@@ -607,15 +607,32 @@
 //세균증식
 // 어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
 
-function solution(n, t) {
-  for (let i = 1; i <= t; i++) {
-    n *= 2;
-  }
+// function solution(n, t) {
+//   for (let i = 1; i <= t; i++) {
+//     n *= 2;
+//   }
 
-  return n;
+//   return n;
+// }
+
+// //다른사람 풀이
+// function solution(n, t) {
+//   return n * Math.pow(2, t); // Math.pow(2, t)는 2의 t승을 계산하는 함수
+// }
+
+//양꼬치
+//머쓱이네 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스로 줍니다. 양꼬치는 1인분에 12,000원, 음료수는 2,000원입니다. 정수 n과 k가 매개변수로 주어졌을 때, 양꼬치 n인분과 음료수 k개를 먹었다면 총얼마를 지불해야 하는지 return 하도록 solution 함수를 완성해보세요.
+
+function solution(n, k) {
+  let totalDrinks = 0;
+  let howMany = Math.floor(n / 10);
+
+  n >= 10 ? (totalDrinks = k - howMany) : (totalDrinks = k);
+
+  return n * 12000 + totalDrinks * 2000;
 }
 
 //다른사람 풀이
-function solution(n, t) {
-  return n * Math.pow(2, t); // Math.pow(2, t)는 2의 t승을 계산하는 함수
+function solution(n, k) {
+  return n * 12000 + k * 2000 - parseInt(n / 10) * 2000;
 }
