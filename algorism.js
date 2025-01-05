28104e189bf427826ae70437616ce91fff91431f
miing_gaeng <pem4134@naver.com>
2025-01-05 17:31:55 +0900

    49 배열더하기해결

--- algorism.js
+++ algorism.js
@@ -587,3 +587,18 @@
 
 //49 두 개 뽑아서 더하기
 // 정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+
+function solution(numbers) {
+  //중복값 없애기 = Set 활용
+  let answer = new Set();
+
+  //[i] + [j] 더한 값 set에 add하기
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      answer.add(numbers[i] + numbers[j]);
+    }
+  }
+
+  //set을 배열로 만들기 + 정렬
+  return Array.from(answer).sort((a, b) => a - b);
+}
