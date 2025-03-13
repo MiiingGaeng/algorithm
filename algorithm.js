0087471c7ffc2ec0c79deb1bf8c5a11bfe877764
miing_gaeng <pem4134@naver.com>
2025-03-13 11:02:12 +0900

    33일차

--- algorithm.js
+++ algorithm.js
@@ -2637,3 +2637,18 @@ function solution(lines) {
 
   return abLength + bcLength - overlap;
 }
+
+//다른 로직으로 구현
+function solution(lines) {
+  let points = Array(200).fill(0);
+
+  // 각 선분이 지나가는 구간을 기록
+  for (let [start, end] of lines) {
+    for (let i = start; i < end; i++) {
+      points[i + 100]++; // 음수 인덱스를 피하기 위해 +100
+    }
+  }
+
+  // 2개 이상 겹치는 부분의 길이를 세기
+  return points.filter((count) => count >= 2).length;
+}
